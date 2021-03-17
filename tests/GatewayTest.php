a0059bcabbb1d75f12cdf705bfd929c82dee5291
James Ellis (DPC) <james.ellis@dpc.nsw.gov.au>
2021-03-17 18:21:10 +1100

    Tests setup

--- tests/GatewayTest.php
+++ tests/GatewayTest.php
@@ -2,13 +2,52 @@
 
 namespace OmniPay\NSWGOVCPP\Tests;
 
-use OmniPay\NSWGOVCPP\CustomerPaymentsPlatformGateway;
-use PHPUnit\Framework\TestCase;
+use Omnipay\Tests\GatewayTestCase;
+use OmniPay\NSWGOVCPP\Gateway;
+use OmniPay\NSWGOVCPP\Exception\AccessTokenRequestException;
+use OmniPay\NSWGOVCPP\Exception\CompleteAccessTokenRequestException;
+use OmniPay\NSWGOVCPP\Exception\CompletePurchaseRequestException;
+use OmniPay\NSWGOVCPP\Exception\RefundRequestException;
+use OmniPay\NSWGOVCPP\Exception\PaymentCompletionException;
+use OmniPay\NSWGOVCPP\Exception\UnprocessableEntityException;
+use OmniPay\NSWGOVCPP\Message\AccessTokenRequest;
+use OmniPay\NSWGOVCPP\Message\CompleteAccessTokenRequest;
+use OmniPay\NSWGOVCPP\Message\CompletePurchaseRequest;
+use OmniPay\NSWGOVCPP\Message\RefundRequest;
 
-class GatewayTest extends TestCase {
+class GatewayTest extends GatewayTestCase {
 
-    public function testAccessToken() {
+    /** @var  Gateway */
+    protected $gateway;
 
+    public function setUp()
+    {
+        parent::setUp();
+        $this->gateway = new Gateway($this->getHttpClient(), $this->getHttpRequest());
+    }
+
+    protected function getTestParams() {
+        return [
+            'clientId' => 'testClientId',
+            'clientSecret' => 'testClientSecret',
+            'jwtSecret' => 'testJWTSecret',
+            'accessTokenUrl' => 'https://localhost/accesstoken',
+            'requestPaymentUrl' => 'https://localhost/paymentrequest',
+            'gatewayUrl' => 'https://localhost/gateway',
+            'refundUrl' => 'https://localhost/refund'
+        ];
+    }
+
+    public function testAuthorise()
+    {
+        $params = $this->getTestParams();
+
+        $request = $this->gateway->authorize([
+            'clientId' => $params['clientId'],
+            'clientSecret' => $params['clientSecret'],
+            'accessTokenUrl' => $params['accessTokenUrl']
+        ]);
+        $this->assertInstanceOf(AccessTokenRequest::class, $request);
     }
 
 }
