3c08c815675767defeb3780e4ddb0f345dc9f0d1
James Ellis (DPC) <james.ellis@dpc.nsw.gov.au>
2021-03-19 18:53:41 +1100

    Refund not void

--- src/Exception/RefundRequestException.php
+++ src/Exception/RefundRequestException.php
@@ -3,7 +3,7 @@
 namespace Omnipay\NSWGOVCPP;
 
 /**
- * Specific exception thrown when a void request cannot be completed
+ * Specific exception thrown when a refund request cannot be completed
  * @author James
  */
 class RefundRequestException extends \Exception {}
