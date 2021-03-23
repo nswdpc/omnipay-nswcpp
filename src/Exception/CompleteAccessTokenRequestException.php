d8a3e6a97edf1498646fadc920554059f43ad8ae
James Ellis (DPC) <james.ellis@dpc.nsw.gov.au>
2021-03-23 11:41:19 +1100

    Refactor tests to handle access token (authorise) moving into purchase() and refund() gateway handling

--- src/Exception/CompleteAccessTokenRequestException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Omnipay\NSWGOVCPP;
-
-/**
- * Specific exception thrown when completing authorization cannot be completed
- * @author James
- */
-class CompleteAccessTokenRequestException extends \Exception {}
