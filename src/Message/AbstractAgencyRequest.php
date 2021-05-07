fb7f28150181a68062d4f2aaa24453e274d307ac
James (DPC) <james.ellis@dpc.nsw.gov.au>
2021-05-07 18:46:38 +1000

    Enhance: handle application/json and application/x-www-form-urlencoded payload encoding. log the JSON error msg if a json_decode() error occurs

--- src/Message/AbstractAgencyRequest.php
+++ src/Message/AbstractAgencyRequest.php
@@ -23,17 +23,30 @@ abstract class AbstractAgencyRequest extends AbstractRequest
             'User-Agent' => "NSWDPC/CPP-Client (Omnipay)"
         ];
         $headers = array_merge($default_headers, $headers);
+        $data = null;
+        switch($headers['Content-Type']) {
+            case 'application/json':
+                $data = json_encode($post_data);
+                break;
+            case 'application/x-www-form-urlencoded':
+                $data = http_build_query($post_data, '', "&", PHP_QUERY_RFC1738);
+                break;
+            default:
+                throw new \Exception("Content-Type must be application/json or application/x-www-form-urlencoded");
+                break;
+        }
+
         $response = $this->httpClient->request(
             "POST",
             $url,
             $headers,
-            json_encode($post_data)
+            $data
         );
         if ($response instanceof ResponseInterface) {
             $data = json_decode($response->getBody(), true, JSON_THROW_ON_ERROR);
             return $data;
         }
-        throw new \Exception("Invalid response from POST request to {$url}");
+        throw new \Exception("Invalid JSON response from POST request to {$url} error=". json_last_error_msg());
     }
 
     /**
