808ca30a235e19889e83ff52617b9c3e4418f473
James (DPC) <james.ellis@dpc.nsw.gov.au>
2021-05-27 15:31:37 +1000

    Return the correct HTTP code as the Exception code, to assist in determining the correct HTTP response

--- src/Exception/JWTDecodeException.php
+++ src/Exception/JWTDecodeException.php
@@ -2,11 +2,16 @@
 
 namespace Omnipay\NSWGOVCPP;
 
+use Symfony\Component\HttpFoundation\Response;
+
 /**
  * Specific exception thrown when the incoming JWT cannot be decoded
- * This results in a 422 exception
+ * This results in a 422 response as it is highly unlikely that further attempts in the CPP timeframe will succeed
  * @author James
  */
 class JWTDecodeException extends UnprocessableEntityException
 {
+    public function getCode() {
+        return Response::HTTP_UNPROCESSABLE_ENTITY;
+    }
 }
--- src/Exception/UnprocessableEntityException.php
+++ src/Exception/UnprocessableEntityException.php
@@ -2,6 +2,8 @@
 
 namespace Omnipay\NSWGOVCPP;
 
+use Symfony\Component\HttpFoundation\Response;
+
 /**
  * Specific exception that can be thrown by an application and caught in
  * CompletePurchaseResponse::complete(), resulting in the required 422 error code being returned to the gateway
@@ -9,4 +11,7 @@ namespace Omnipay\NSWGOVCPP;
  */
 class UnprocessableEntityException extends \Exception
 {
+    public function getCode() {
+        return Response::HTTP_UNPROCESSABLE_ENTITY;
+    }
 }
